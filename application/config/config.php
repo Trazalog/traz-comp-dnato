8aa699189687aa6e57414c48c4586f73c9ce3665
Unknown <fleiva@trazalog.com>
2020-05-07 14:22:41 -0300

    Testt

--- application/config/config.php
+++ application/config/config.php
@@ -403,7 +403,7 @@ $config['sess_regenerate_destroy'] = FALSE;
 |
 */
 $config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
+$config['cookie_domain']	= 'https://www.trazalog.com.ar';
 $config['cookie_path']		= '/';
 $config['cookie_secure']	= FALSE;
 $config['cookie_httponly'] 	= FALSE;
