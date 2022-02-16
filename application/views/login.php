6165b3c27e0fb966d9272cdb8278e6f7068cbb2c
RRuiz <rruiz@xn--razalog-2rb.com>
2022-02-16 18:20:40 -0300

    Nueva home page con tema rojo

--- application/views/login.php
+++ application/views/login.php
@@ -1,5 +1,16 @@
 <div class="col-lg-4 col-lg-offset-4">
-    <h2>Bienvenido!</h2>
+<style>
+body {
+  background-image: url('<?php echo base_url();?>public/img/toolsbkg.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+</style>
+<br>
+<br>
+<img src="<?php echo base_url();?>public/img/logotzl.png" alt="Trazalog Tools" class="brand-image" style="width: 360px; height: auto !important;">
+<br>
+    <h2>¡Bienvenido!</h2>
     <h5>Ingrese por favor.</h5>
     <?php $fattr = array('class' => 'form-signin');
          echo form_open(base_url().'main/login/', $fattr); ?>
